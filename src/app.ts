acf5a73c1c543ceb57c74289f7c76299aa9e1757
robert-allen <allen.robert79@gmail.com>
2024-02-16 17:59:29 +0000

    completes tutorial 12

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+}
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
