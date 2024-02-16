575f0d3407fc78c9aaa115a51abae189d913a870
robert-allen <allen.robert79@gmail.com>
2024-02-16 12:17:38 +0000

    completes tutorial 1, 2 and 3

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+var character = 'mario';
+var age = 30;
+var isBackBelt = false;
+character = 'luigi';
+age = 40;
+isBackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+let character = 'mario';
+let age = 30;
+let isBackBelt = false;
+
+character = 'luigi';
+
+age = 40;
+
+isBackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circ(7.5))
